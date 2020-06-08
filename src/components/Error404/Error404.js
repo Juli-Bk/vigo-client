e936a7dcc95f7aadd05dab34d55f1e740f827b32
Stan Bondar <bondar.stan@gmail.com>
2020-06-08 23:10:04 +0300

    checking commit

--- src/components/Error404/Error404.js
+++ src/components/Error404/Error404.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import { Container, Grid, CardMedia, Typography } from '@material-ui/core';
+import React, {useEffect} from 'react';
+import { Container, Grid, CardMedia, Typography, Box } from '@material-ui/core';
 import useStyles from './Error404Style';
 
 const Error404 = () => {
   const styles = useStyles();
 
+  useEffect(() => {
+    if (document.readyState === 'complete') {
+      document.getElementById('free-solo-dialog-demo').focus();
+    }
+  }, []);
   return (
     <Container>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={5}>
+      <Grid container className={styles.box}>
+        <Grid item xs={11} sm={6}>
           <Box className={styles.sorryBlock}>
-            <Typography className={}>
-
+            <Typography className={styles.sorryTitle} variant='h1'>
+              We’re Sorry...
             </Typography>
+            <Typography className={styles.secondLine} variant='body1'>This page cannot be found.</Typography>
+            <Typography className={styles.thirdLine} variant='body2'>Maybe you want to perform a search?</Typography>
           </Box>
         </Grid>
-        <Grid item xs={12} sm={7}>
+        <Grid item xs={11} sm={5}>
           <CardMedia
             className={styles.image}
             image='/img/404/dog_2.png'
@@ -26,4 +33,4 @@ const Error404 = () => {
   );
 };
 
-export default Error404;
+export default React.memo(Error404);
--- src/components/Error404/Error404Style.js
+++ src/components/Error404/Error404Style.js
@@ -3,8 +3,71 @@ import { colors } from '../../styles/colorKit';
 import { fonts } from '../../styles/fonts/fontsKit';
 
 const useStyles = makeStyles(theme => ({
+  box: {
+    justifyContent: 'center',
+    [theme.breakpoints.up('sm')]: {
+      flexDirection: 'row-reverse',
+      justifyContent: 'space-around'
+    }
+  },
   sorryBlock: {
-    padding: 20
+    maxWidth: 570,
+    padding: 20,
+    backgroundColor: colors.bg404Message,
+    borderRadius: 20,
+    [theme.breakpoints.up(760)]: {
+      padding: 30
+    },
+    [theme.breakpoints.up('md')]: {
+      padding: 35
+    },
+    [theme.breakpoints.up('lg')]: {
+      padding: '55px 45px'
+    }
+  },
+  sorryTitle: {
+    fontSize: '1.88rem',
+    fontFamily: fonts.f3,
+    textTransform: 'uppercase',
+    color: colors.bgSecondary,
+    [theme.breakpoints.up(760)]: {
+      fontSize: '2.5rem'
+    },
+    [theme.breakpoints.up('md')]: {
+      fontSize: '3.75rem'
+    },
+    [theme.breakpoints.up('lg')]: {
+      fontSize: '4.38rem'
+    }
+  },
+  secondLine: {
+    fontSize: '.94rem',
+    fontFamily: fonts.f4,
+    color: colors.bgSecondary,
+    marginBottom: '0.94rem',
+    [theme.breakpoints.up(760)]: {
+      marginBottom: '1.44rem',
+      fontSize: '1.25rem'
+    },
+    [theme.breakpoints.up('md')]: {
+      marginBottom: '1.88rem',
+      fontSize: '1.69rem'
+    },
+    [theme.breakpoints.up('lg')]: {
+      marginBottom: '1.19rem',
+      fontSize: '1.88rem'
+    }
+  },
+  thirdLine: {
+    fontSize: '0.75rem',
+    color: colors.fontFourth,
+    fontFamily: fonts.f2,
+    [theme.breakpoints.up('sm')]: {
+      fontSize: '0.94rem'
+    },
+    [theme.breakpoints.up('md')]: {
+      fontSize: '1.13rem'
+    }
   },
   image: {
     width: '100%',
