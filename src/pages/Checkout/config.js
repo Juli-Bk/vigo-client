ed7998dec32a5cd3c47a222ff813ae83490ad555
Anna Lypovenko <anna.lypovenko@gmail.com>
2020-06-12 11:10:33 +0300

    bug fix

--- src/pages/Checkout/config.js
+++
@@ -1,9 +0,0 @@
-
-export const deliveryOptions = {
-  Vigo_Courier_Service: 'Vigo Courier Service',
-  Nova_Poshta: 'Nova Poshta',
-  DHL_Express: 'DHL Express',
-  FedEx: 'FedEx',
-  Pickup: 'Pickup',
-  Post_Office: 'Post Office'
-};
