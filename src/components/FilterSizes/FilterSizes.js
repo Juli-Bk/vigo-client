826a4eed7ef4959162a6edfb4b1a933b1c49dc21
Лариса Чепурная <alisa444.86@gmail.com>
2020-06-13 19:19:34 +0300

    FilterSizes.js logic of request created

--- src/components/FilterSizes/FilterSizes.js
+++ src/components/FilterSizes/FilterSizes.js
@@ -11,22 +11,22 @@ const FilterSizes = (props) => {
   const [sizes, setSizes] = useState([]);
   const [sizeTypes, setSizeTypes] = useState([]);
 
+  const sizesTree = {};
+  sizeTypes.length && sizeTypes.map(type => {
+    return sizesTree[type] = sizes.filter(obj => obj.sizeType === type);
+  });
+  console.log(sizesTree);
+
   useEffect(() => {
     AjaxUtils.Sizes.getAllSizes()
       .then(result => {
-        console.log(result);
         setSizes(result.items);
 
         const sizesSet = new Set();
         result.items.forEach(size => {
           sizesSet.add(size.sizeType);
         });
-
         setSizeTypes(Array.from(sizesSet));
-        const sizesTree = {};
-        sizeTypes.map(type => {
-          sizesTree[type] = [];
-        });
       });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
