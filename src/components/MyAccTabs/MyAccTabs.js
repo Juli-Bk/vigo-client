d47ab2b40cb8f8b3a9f87fae58ddcd133106c07b
Alisa44 <alisa444.86@gmail.com>
2020-06-18 17:52:49 +0300

    Package.json clearing (#43)
    * cleared package.json, conditional render ShowBy, changed ProductsTable.js
    
    * react-router restored
    
    * onstant deleted

--- src/components/MyAccTabs/MyAccTabs.js
+++ src/components/MyAccTabs/MyAccTabs.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { makeStyles, ThemeProvider, useTheme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import themeMui from './MyAccTabsTheme';
 import PersonalDetailsForm from '../PersonalDetailsForm/PersonalDetailsForm';
@@ -13,7 +15,7 @@ import { useMediaQuery } from '@material-ui/core';
 import AjaxUtils from '../../ajax';
 
 const TabPanel = (props) => {
-  const { children, value, index, ...other } = props;
+  const { user, children, value, index, ...other } = props;
 
   return (
     <Box
@@ -25,7 +27,7 @@ const TabPanel = (props) => {
     >
       {value === index && (
         <Box p={3}>
-          {children}
+          <Typography component='span'>{children}</Typography>
         </Box>
       )}
     </Box>
@@ -102,7 +104,7 @@ const MyAccTabs = (props) => {
           <Wishlist />
         </TabPanel>
         <TabPanel value={value} index={3} dir={theme.direction}>
-          Purchase history
+        Purchase history
         </TabPanel>
       </ThemeProvider>
     </Box>
@@ -115,4 +117,10 @@ TabPanel.propTypes = {
   value: PropTypes.any.isRequired
 };
 
-export default React.memo(MyAccTabs);
+const mapStateToProps = store => {
+  return {
+    user: store.user
+  };
+};
+
+export default React.memo(connect(mapStateToProps)(MyAccTabs));
