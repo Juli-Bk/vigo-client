b014b01bb970bb4ef4d8e6d8b88c26895c2f906d
Stan Bondar <bondar.stan@gmail.com>
2020-06-22 19:30:44 +0300

    Solved problem with staying on footer after clicking by any link

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -17,33 +17,36 @@ import Returns from '../pages/Returns/Returns';
 import Shipping from '../pages/Shipping/Shipping';
 import store from './../redux/store';
 import {setLoginModalOpenState} from '../redux/actions/actions';
+import AutoScrollTop from '../components/AutoScrollTop/AutoScrollTop';
 
 const AppRoutes = () => {
   return (
     <>
       <Route path='/:page?' component={Header}/>
-      <Switch>
-        <Route exact path='/' component={Home}/>
+      <AutoScrollTop >
+        <Switch>
+          <Route exact path='/' component={Home}/>
 
-        <Route exact path='/products/filter' component={Products}/>
-        <Route exact path='/products/:id' component={Product}/>
-        <Route exact path='/products' component={Products}/>
+          <Route exact path='/products/filter' component={Products}/>
+          <Route exact path='/products/:id' component={Product}/>
+          <Route exact path='/products' component={Products}/>
 
-        <Route exact path='/compare' component={ProductCompare}/>
-        <Route exact path='/wishlist' component={Wishlist}/>
-        <Route exact path='/cart' component={ShoppingCart}/>
-        <Route exact path='/checkout' component={Checkout}/>
-        <Route exact path='/contacts' component={Contacts}/>
-        <Route exact path='/privacyPolicy' component={PrivacyPolicy}/>
-        <Route exact path='/returns' component={Returns}/>
-        <Route exact path='/shipping' component={Shipping}/>
+          <Route exact path='/compare' component={ProductCompare}/>
+          <Route exact path='/wishlist' component={Wishlist}/>
+          <Route exact path='/cart' component={ShoppingCart}/>
+          <Route exact path='/checkout' component={Checkout}/>
+          <Route exact path='/contacts' component={Contacts}/>
+          <Route exact path='/privacyPolicy' component={PrivacyPolicy}/>
+          <Route exact path='/returns' component={Returns}/>
+          <Route exact path='/shipping' component={Shipping}/>
 
-        <ProtectedRoute
-          authenticated={!!getJWTfromCookie()}
-          exact path='/account'
-          component={MyAccount}/>
-        <Route path='*' component={Page404}/>
-      </Switch>
+          <ProtectedRoute
+            authenticated={!!getJWTfromCookie()}
+            exact path='/account'
+            component={MyAccount}/>
+          <Route path='*' component={Page404}/>
+        </Switch>
+      </AutoScrollTop>
     </>
   );
 };
