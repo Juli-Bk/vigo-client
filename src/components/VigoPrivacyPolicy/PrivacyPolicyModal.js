ad61640cf4a92e3d4a87f70a36314f46807e76d2
Anna Lypovenko <anna.lypovenko@gmail.com>
2020-07-03 01:29:22 +0300

    added radiobox to account, styles changed

--- src/components/VigoPrivacyPolicy/PrivacyPolicyModal.js
+++ src/components/VigoPrivacyPolicy/PrivacyPolicyModal.js
@@ -9,32 +9,7 @@ import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Slide from '@material-ui/core/Slide';
 import VigoPrivacyPolicy from './VigoProvacyPolicy';
-import { colors } from '../../styles/colorKit';
-import { fonts } from '../../styles/fonts/fontsKit';
-
-const useStyles = makeStyles((theme) => ({
-  appBar: {
-    position: 'relative',
-    backgroundColor: 'black'
-  },
-  title: {
-    marginLeft: theme.spacing(2),
-    flex: 1
-  },
-  linkButton: {
-    marginTop: '1.25rem',
-    color: colors.fontSecondary,
-    fontWeight: 300,
-    fontFamily: fonts.f3,
-    border: 'none',
-    fonFamily: fonts.f2,
-    textTransform: 'capitalize',
-    '&:hover': {
-      backgroundColor: 'none',
-      color: colors.noticeColor
-    }
-  }
-}));
+import useStyles from '../../styles/formStyle/formStyle';
 
 const Transition = forwardRef(function Transition (props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -63,12 +38,13 @@ const PrivacyPolicyModal = () => {
             <IconButton edge="start" onClick={handleClose} aria-label="close">
               <CloseIcon />
             </IconButton>
-            <Button autoFocus color="inherit" onClick={handleClose}>
-              ok
-            </Button>
+
           </Toolbar>
         </AppBar>
         <VigoPrivacyPolicy />
+        <Button autoFocus className={classes.buttonOk} color="inherit" onClick={handleClose}>
+        ok
+        </Button>
       </Dialog>
     </Box>
   );
