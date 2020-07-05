b4479f8d8a3429387305e8ead96e58f52c2fa181
Stan Bondar <bondar.stan@gmail.com>
2020-07-05 20:56:20 +0300

    removed to-do

--- src/redux/actions/sizeTable/index.js
+++ src/redux/actions/sizeTable/index.js
@@ -5,10 +5,8 @@ import {setLoading} from '../actions';
 export const getSizeTable = (id) => dispatch => {
   dispatch(setLoading(true));
   AjaxUtils.SizeTables.getSizeTableByProductId(id)
-  // to-do set loading proccess
     .then(result => {
       console.log('Result from redux', result);
-      // to do turn-off loading proccess
       dispatch(setLoading(false));
       if (result) {
         dispatch({type: Actions.SET_SIZE_TABLE, payload: result});
