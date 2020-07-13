6e7f3617b15e3cc7d9aeee8aa8a4ef6b146d307e
Anna Lypovenko <anna.lypovenko@gmail.com>
2020-07-13 22:58:57 +0300

    provacy policy list fixed

--- src/components/VigoPrivacyPolicy/VigoPrivacyPolicyStyle.js
+++ src/components/VigoPrivacyPolicy/VigoPrivacyPolicyStyle.js
@@ -9,13 +9,15 @@ const useStyles = makeStyles(() => ({
     fontFamily: fonts.f2,
     color: colors.fontPrimary,
     marginBottom: '1rem',
-    textTransform: 'uppercase'
+    textTransform: 'uppercase',
+    margin: 10
   },
   text: {
     fontWeight: '400',
     fontFamily: fonts.f2,
     color: colors.fontPrimary,
-    marginBottom: '.63rem'
+    marginBottom: '.63rem',
+    margin: 15
   }
 }));
 
